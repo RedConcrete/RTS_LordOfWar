0efa49cb3a9e2fc56a071eb5e64b38983d78ec87
Cem Arslan <cearslan@stud.hs-heilbronn.de>
2021-04-04 16:17:54 +0200

    SERVER added enum PointRewards (for saving point rewards) 	-added enums for killing villagers,soldiers,castles and winning and losing games

---
+++ Code_Server/LordOfWarServer/src/main/java/de/constants/PointRewards.java
@@ -0,0 +1,15 @@
+package de.constants;
+
+public enum PointRewards {
+    KILL_VILLAGER(50), KILL_SOLDIER(100),DESTROY_CASTLE(1000),
+    WIN_GAME(10000), LOSE_GAME(0);
+    private final int pointReward;
+
+    PointRewards(int reward) {
+        this.pointReward = reward;
+    }
+
+    public int getPointReward() {
+        return pointReward;
+    }
+}
