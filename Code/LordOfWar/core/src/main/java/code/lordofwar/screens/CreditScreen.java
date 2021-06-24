c9c2af888665db753a7ca2142e146a224ebb89e0
Robin Hefner <rohefner@stud.hs-heilbronn.de>
2021-06-24 15:12:34 +0200

    Added Credits to CreditScreen

--- Code/LordOfWar/core/src/main/java/code/lordofwar/screens/CreditScreen.java
+++ Code/LordOfWar/core/src/main/java/code/lordofwar/screens/CreditScreen.java
@@ -3,9 +3,9 @@ package code.lordofwar.screens;
 import code.lordofwar.main.LOW;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+
+import javax.swing.*;
 
 public class CreditScreen extends Screens implements Screen {
 
@@ -53,7 +53,28 @@ public class CreditScreen extends Screens implements Screen {
 
         Window creditWindow = new Window("",skin,"border");
 
+        Label headline = new Label("Programmer",skin);
+        headline.setFontScale(3f);
+        Label franz = new Label("Franz Klose",skin);
+        Label cem = new Label("Cem Arslan",skin);
+        Label robin = new Label("Robin Hefner",skin);
+        Label mehdy = new Label("Mehdy Abbas",skin);
+        Label assetDesigner = new Label("Asset Designer",skin);
+        assetDesigner.setFontScale(3f);
+        Label glebster51 = new Label("glebster51 | OpenGameArt.org",skin);
+
+
+
+
         creditWindow.defaults().pad(20f);
+        creditWindow.add(headline).row();
+        creditWindow.add(franz).row();
+        creditWindow.add(cem).row();
+        creditWindow.add(robin).row();
+        creditWindow.add(mehdy).row();
+        creditWindow.add(assetDesigner).row();
+        creditWindow.add(glebster51).row();
+
 
         backButton(stage, skin, game, creditWindow);
         packAndSetWindow(creditWindow, stage);
